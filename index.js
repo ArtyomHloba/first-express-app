4f92f8774416a29e00bce0bd52a64b0fab4ab666
ArtyomHloba <hlobartyom@gmail.com>
2024-09-05 13:14:00 +0300

    Add express + nodemon

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-const http = require('http');
-const fs = require('node:fs');
+const http = require('node:http');
+const express = require('express');
 
-const server = http.createServer((request, response) => {
-  console.log(request.url);
-  console.log('', request.method);
+const PORT = process.env.PORT ?? 5000;
 
-  fs.readFile('./pages/index.html', { encoding: 'utf-8' }, (err, file) => {
-    if (err) {
-      response.statusCode = 500;
-      return response.end('Read file error');
-    }
+const app = express();
 
-    response.statusCode = 200;
-    response.setHeader('Content-Tipe', 'text.html');
-    response.end(file);
-  });
+app.get('/', (request, response) => {
+  response.status(200).send('Hello world');
 });
 
-server.listen(5000, '127.0.0.1', () => {
-  console.log('Server is listening http://127.0.0.1:5000');
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server is listening http://127.0.0.1:${PORT}`);
 });
