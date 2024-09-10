da8424ad62e3e7dc28f7718eccdea2e2b69c15f1
artem hloba <artemgloba2@gmail.com>
2024-09-10 16:28:08 +0300

    Fix id

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,76 +1,72 @@
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid')
 
 const tasks = [
   {
-    id: 1,
-    title: 'Зробити домашку',
+    id: '1',
+    title: 'Зробити домашку'
   },
   {
     id: uuidv4(),
-    title: 'Поїсти',
+    title: 'Поїсти'
   },
   {
     id: uuidv4(),
-    title: 'Прогулка з собакою',
+    title: 'Прогулка з собакою'
   },
   {
     id: uuidv4(),
-    title: 'Поспати',
-  },
-];
+    title: 'Поспати'
+  }
+]
 
 module.exports.createTasks = (req, res) => {
-  const { body } = req;
-  tasks.push({ ...body, id: uuidv4() });
+  const { body } = req
+  tasks.push({ ...body, id: uuidv4() })
 
-  res.status(201).send(tasks[tasks.length - 1]);
-};
+  res.status(201).send(tasks[tasks.length - 1])
+}
 
 module.exports.getTasks = (req, res) => {
-  res.status(200).send(tasks);
-};
+  res.status(200).send(tasks)
+}
 
 module.exports.getTaskById = (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
 
-  const foundTask = tasks.find(t => t.id === id || t.id === Number(id));
+  const foundTask = tasks.find(t => t.id === id)
 
   if (!foundTask) {
-    return res.status(404).send('Task not found');
+    return res.status(404).send('Task not found')
   }
 
-  res.status(200).send(foundTask);
-};
+  res.status(200).send(foundTask)
+}
 
 module.exports.updateTaskById = (req, res) => {
   const {
     body,
-    params: { id },
-  } = req;
+    params: { id }
+  } = req
 
-  const foundTaskIndex = tasks.findIndex(
-    t => t.id === id || t.id === Number(id)
-  );
+  const foundTaskIndex = tasks.findIndex(t => t.id === id)
 
   if (foundTaskIndex === -1) {
-    return res.status(404).send('Task not found');
+    return res.status(404).send('Task not found')
   }
 
-  tasks[foundTaskIndex] = { ...tasks[foundTaskIndex], ...body };
-  res.status(200).send(tasks[foundTaskIndex]);
-};
+  tasks[foundTaskIndex] = { ...tasks[foundTaskIndex], ...body }
+  res.status(200).send(tasks[foundTaskIndex])
+}
 
 module.exports.deleteTaskById = (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
 
-  const foundTaskIndex = tasks.findIndex(
-    t => t.id === id || t.id === Number(id)
-  );
+  const foundTaskIndex = tasks.findIndex(t => t.id === id)
 
   if (foundTaskIndex === -1) {
-    return res.status(404).send('Task not found');
+    return res.status(404).send('Task not found')
   }
 
-  tasks.splice(foundTaskIndex, 1);
-  res.status(204).end();
-};
+  tasks.splice(foundTaskIndex, 1)
+  res.status(204).end()
+}
